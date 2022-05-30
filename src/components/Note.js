44281532d08c0e3f1ce7190ab0559704e4046656
William Feng <williamfeng2001@gmail.com>
2022-05-30 12:50:29 +1000

    Added option to change the background colour of notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 import "./Note.css";
 
 function Note({ id, text, date, handleDeleteNote }) {
+  const [color, setColor] = useState("#ffffcc");
+
   return (
-    <div className="note">
+    <div className="note" style={{ backgroundColor: color }}>
       <p>{text}</p>
       <div className="note-footer">
         <small className="extra-info">Last modified: {date}</small>
+        <input
+          type={"color"}
+          value={color}
+          id={id}
+          className="color-label"
+          onChange={() => setColor(document.getElementById(id).value)}
+        />
         <button className="delete-button" onClick={() => handleDeleteNote(id)}>
           <MdDelete />
         </button>
